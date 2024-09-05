298a0872a194f0914e28184adb0e02bd55e2aa66
heyulong <1183851628@qq.com>
2024-09-05 21:20:58 +1000

    controlling the player (#1)
    * Some work on controlling the player
    
    1. Moving the player
    2. Using the character controller
    3. Moving based on rotation
    
    * update
    
    * complete character controller.
    
    ---------
    
    Co-authored-by: yulonghe <yulonghe@global.tencent.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 8;
+    public float jumpForce = 17;
+    public float gravityScale = 5f;
+    public float rotateSpeed = 10f;
+    public CharacterController character;
+    public Animator anim;
+    private float yStore;
+    private CameraController cam;
+    private Vector3 moveAmount;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<CameraController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!character.isGrounded)
+        {
+            moveAmount.y += Physics.gravity.y * gravityScale * Time.fixedDeltaTime;
+        } else
+        {
+            moveAmount.y = Physics.gravity.y * gravityScale * Time.fixedDeltaTime;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        yStore = moveAmount.y;
+
+        moveAmount = cam.transform.forward * Input.GetAxisRaw("Vertical") + (cam.transform.right * Input.GetAxisRaw("Horizontal"));
+        moveAmount.y = 0;
+        moveAmount = moveAmount.normalized;
+
+        if (moveAmount.magnitude > .1f)
+        {
+            if (moveAmount != Vector3.zero)
+            {
+                 Quaternion newRot = Quaternion.LookRotation(moveAmount);
+				 transform.rotation = Quaternion.Slerp(transform.rotation, newRot, rotateSpeed * Time.deltaTime);
+            }
+        }
+
+        moveAmount.y = yStore;
+
+        if (character.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveAmount.y = jumpForce;
+            }
+        }
+
+        character.Move(new Vector3(moveAmount.x * moveSpeed, moveAmount.y, moveAmount.z * moveSpeed) * Time.deltaTime);
+
+        float moveVel = new Vector3(moveAmount.x, 0f, moveAmount.z).magnitude * moveSpeed;
+        anim.SetFloat("speed", moveVel);
+        anim.SetBool("isGrounded", character.isGrounded);
+        anim.SetFloat("yVel", moveAmount.y);
+    }
+}
