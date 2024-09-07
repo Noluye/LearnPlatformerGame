fdb28b6e8604346e726dcf420bd88b0ae23285c9
Noluye <1183851628@qq.com>
2024-09-07 10:25:15 +1000

    Feature: Player Health System
    * Taking damage
    * Invincibility

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+    private int currentHealth;
+    public int maxHealth = 5;
+    public float invinciblityLength = 1.7f;
+    private float invinceCounter;
+
+    public GameObject[] modelDisplay;
+    private float flashCounter;
+    public float flashTimeInterval = .1f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FillHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invinceCounter > 0)
+        {
+            invinceCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0)
+            {
+                flashCounter = flashTimeInterval;
+                foreach (GameObject piece in modelDisplay)
+                {
+                    piece.SetActive(!piece.activeSelf);
+                }
+            }
+
+            // last frame
+            if (invinceCounter <= 0)
+            {
+                foreach (GameObject piece in modelDisplay)
+                {
+                    piece.SetActive(true);
+                }
+            }
+        }
+
+        
+    }
+
+    public void DamagePlayer()
+    {
+        if (invinceCounter > 0) return;
+
+        invinceCounter = invinciblityLength;
+
+        currentHealth--;
+        if (currentHealth <= 0)
+        {
+            LevelManager.instance.Respawn();
+        }
+    }
+
+    public void FillHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
---
+++ Reflection.md
@@ -0,0 +1,9 @@
+# Reflection on the game development
+
+
+
+## Psychology
+
+I learn many psychology through online learning. Those will affect the design of our game. For example:
+
+- The obstacle damage: The player tend to feel good and get a sense of control when making the collider a little bit smaller than the visual size to damage the player. 
